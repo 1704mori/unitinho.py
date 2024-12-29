4c4c17e331fb655c72b7f380087a050f0223d40b
1704mori <1704mori@gmail.com>
2024-12-29 20:19:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+```bash
+$ python -m venv .venv
+
+# windows
+.venv\Scripts\activate
+
+# unix 
+source .venv/bin/activate
+
+# then
+pip install -r requirements.txt
+
+# and
+python main.py
+
+# we use BOT_TOKEN and BOT_PREFIX environment variables.
+```
---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+cogs: list = ["cogs.music.music"]
+
+client = commands.Bot(command_prefix=os.getenv("BOT_PREFIX"), help_command=None, intents=intents)
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('prefix: .'))
+
+    for cog in cogs:
+        try:
+            print(f"Loading cog {cog}")
+            await client.load_extension(cog)
+        except Exception as e:
+            exc = "{}: {}".format(type(e).__name__, e)
+            print("Failed to load cog {}\n{}".format(cog, exc))
+        else:
+            print(f"Loaded cog {cog}")
+    
+    print("bot is ready")
+
+client.run(os.getenv("BOT_TOKEN"))
