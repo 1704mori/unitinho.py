21b9472706c320b669b19fc7b36ac4897e773166
1704mori <1704mori@gmail.com>
2024-12-30 17:35:01 -0300

    add help command

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ intents = discord.Intents.all()
 cogs: list = ["cogs.music.music"]
 
 client = commands.Bot(command_prefix=os.getenv("BOT_PREFIX"), help_command=None, intents=intents)
+client.remove_command('help')
 
 @client.event
 async def on_ready():
@@ -31,5 +32,37 @@ async def on_ready():
 async def sync(ctx: commands.Context):
     synced = await ctx.bot.tree.sync()
     await ctx.send(f'Command tree synced {len(synced)} commands.')
+
+
+@client.command()
+async def help(ctx, args=None):
+    help_embed = discord.Embed(title="My Bot's Help!")
+    command_names_list = [x.name for x in client.commands]
+
+    if not args:
+        help_embed.add_field(
+            name="List of supported commands:",
+            value="\n".join([str(i+1)+". "+x.name for i,x in enumerate(client.commands)]),
+            inline=False
+        )
+        help_embed.add_field(
+            name="Details",
+            value="Type `.help <command name>` for more details about each command.",
+            inline=False
+        )
+
+    elif args in command_names_list:
+        help_embed.add_field(
+            name=args,
+            value=client.get_command(args).help
+        )
+
+    else:
+        help_embed.add_field(
+            name="Nope.",
+            value="Don't think I got that command, boss!"
+        )
+
+    await ctx.send(embed=help_embed)
 
 client.run(os.getenv("BOT_TOKEN"))
